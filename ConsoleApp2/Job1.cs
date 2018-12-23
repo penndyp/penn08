3c507c1b589027bdcf4869a57c478700acd6c1b2
penn <273990685@qq.com>
2018-12-23 12:53:09 +0800

    12222222222222

--- ConsoleApp2/Job1.cs
+++ ConsoleApp2/Job1.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp2
         private static Logger logger = NLog.LogManager.GetCurrentClassLogger();
         public void Execute(IJobExecutionContext context)
         { 
+            //123
             var statrt = string.Format("{0:yyyy-MM-dd} 18:00:00", DateTime.Now.AddDays(-1));
             var end = string.Format("{0:yyyy-MM-dd} 18:00:00", DateTime.Now);
             logger.Error(string.Format("start:{0},end:{1}", statrt, end));
